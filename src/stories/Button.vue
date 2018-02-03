a5bacedc024c546a1366ab371c9f16ecdcb85918
limichange <limichange@hotmail.com>
2018-02-03 09:21:49 +0800

    Init storybook

---
+++ src/stories/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.Button">
+    Button
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" module>
+.Button {}
+</style>
